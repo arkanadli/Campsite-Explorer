2b948b9f2451a12cbf4335eaf7287df48ed108b8
arkanadli <uzzomaki@gmail.com>
2023-10-22 17:15:16 +0700

    Update index.js

--- Production 1.0.0 ( First Deploying )/cloudinary/index.js
+++ Production 1.0.0 ( First Deploying )/cloudinary/index.js
@@ -10,7 +10,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
     cloudinary,
     params: {
-        folder: 'Campsite Explorer',
+        folder: 'Campsite   Explorer',
         allowedFormat: ['jpeg', 'png', 'jpg']
     }
 });
